6a22deaba591af46dc8534f298f137157cff22fe
Hasan Ragab Eltantawy <50374022+HasanEltantawy@users.noreply.github.com>
2022-02-07 03:38:39 +0200

    App in portrait mode only

--- hisnelmoslem/lib/main.dart
+++ hisnelmoslem/lib/main.dart
@@ -62,6 +62,10 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
     return ListenableProvider(
       create: (context) => AppSettingsNotifier(),
       child: GetMaterialApp(
