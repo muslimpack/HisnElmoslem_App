9862515ba66b9982449d46885814d8d4febe7e0f
Hasan Ragab Eltantawy <hasaneltantawy@gmail.com>
2024-02-19 21:06:36 +0200

    initServices

--- hisnelmoslem/lib/app/init_services.dart
+++ hisnelmoslem/lib/app/init_services.dart
@@ -1,4 +1,6 @@
 import 'package:awesome_notifications/awesome_notifications.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get_storage/get_storage.dart';
 import 'package:hisnelmoslem/app/shared/functions/print.dart';
 import 'package:hisnelmoslem/src/core/utils/alarm_manager.dart';
@@ -7,6 +9,14 @@ import 'package:hisnelmoslem/src/core/utils/migration/migration.dart';
 import 'package:hisnelmoslem/src/core/utils/notification_manager.dart';
 
 Future<void> initServices() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setSystemUIOverlayStyle(
+    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
+  );
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
   try {
     await GetStorage.init();
     await Migration.start();
--- hisnelmoslem/lib/main.dart
+++ hisnelmoslem/lib/main.dart
@@ -1,18 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:hisnelmoslem/app/app.dart';
 import 'package:hisnelmoslem/app/init_services.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
   await initServices();
-  SystemChrome.setSystemUIOverlayStyle(
-    const SystemUiOverlayStyle(statusBarColor: Colors.transparent),
-  );
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
 
   runApp(const MyApp());
 }
