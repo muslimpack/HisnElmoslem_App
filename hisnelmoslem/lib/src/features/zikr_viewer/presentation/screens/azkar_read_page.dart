1c1ae66c21893f40f2883be6ec2f52160b126f9f
Hasan Ragab Eltantawy <hasaneltantawy@gmail.com>
2024-02-20 08:04:53 +0200

    separate bottom bar

--- hisnelmoslem/lib/src/features/zikr_viewer/presentation/screens/azkar_read_page.dart
+++ hisnelmoslem/lib/src/features/zikr_viewer/presentation/screens/azkar_read_page.dart
@@ -2,12 +2,11 @@ import 'package:clipboard/clipboard.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hisnelmoslem/src/core/repos/app_data.dart';
-import 'package:hisnelmoslem/src/core/shared/widgets/font_settings.dart';
 import 'package:hisnelmoslem/src/core/shared/widgets/loading.dart';
-import 'package:hisnelmoslem/src/core/utils/email_manager.dart';
 import 'package:hisnelmoslem/src/core/values/constant.dart';
 import 'package:hisnelmoslem/src/features/home/presentation/controller/dashboard_controller.dart';
 import 'package:hisnelmoslem/src/features/zikr_viewer/presentation/components/zikr_viewer_page_mode_appbar.dart';
+import 'package:hisnelmoslem/src/features/zikr_viewer/presentation/components/zikr_viewer_page_mode_bottom_bar.dart';
 import 'package:hisnelmoslem/src/features/zikr_viewer/presentation/controller/azkar_read_page_controller.dart';
 
 class AzkarReadPage extends StatelessWidget {
@@ -180,58 +179,11 @@ class AzkarReadPage extends StatelessWidget {
                     );
                   },
                 ),
-                bottomNavigationBar: BottomAppBar(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: <Widget>[
-                      Expanded(
-                        child: IconButton(
-                          splashRadius: 20,
-                          padding: EdgeInsets.zero,
-                          icon: const Icon(Icons.copy),
-                          onPressed: () {
-                            FlutterClipboard.copy("${text!}\n${fadl!}")
-                                .then((result) {
-                              final snackBar = SnackBar(
-                                content: Text("copied to clipboard".tr),
-                                action: SnackBarAction(
-                                  label: "done".tr,
-                                  onPressed: () {},
-                                ),
-                              );
-
-                              ScaffoldMessenger.of(context)
-                                  .showSnackBar(snackBar);
-                            });
-                          },
-                        ),
-                      ),
-                      Expanded(
-                        flex: 3,
-                        child: FontSettingsToolbox(
-                          controllerToUpdate: controller,
-                        ),
-                      ),
-                      Expanded(
-                        child: IconButton(
-                          splashRadius: 20,
-                          padding: EdgeInsets.zero,
-                          icon: const Icon(
-                            Icons.report,
-                            color: Colors.orange,
-                          ),
-                          onPressed: () {
-                            EmailManager.sendMisspelledInZikrWithText(
-                              subject: controller.zikrTitle!.name,
-                              cardNumber: cardNumber.toString(),
-                              text: text!,
-                            );
-                          },
-                        ),
-                      ),
-                    ],
-                  ),
+                bottomNavigationBar: ZikrViewerPageModeBottomBar(
+                  controller: controller,
+                  text: text,
+                  fadl: fadl,
+                  cardNumber: cardNumber,
                 ),
               );
       },
