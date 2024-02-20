45049290b1e60ecfce81231479e80106be68f30f
Hasan Ragab Eltantawy <hasaneltantawy@gmail.com>
2024-02-20 21:13:46 +0200

    Update range_text_formatter.dart

--- hisnelmoslem/lib/src/core/utils/range_text_formatter.dart
+++ hisnelmoslem/lib/src/core/utils/range_text_formatter.dart
@@ -1,5 +1,3 @@
-import 'dart:math';
-
 import 'package:hisnelmoslem/src/core/functions/print.dart';
 import 'package:hisnelmoslem/src/features/quran/data/models/verse_range.dart';
 
@@ -8,28 +6,18 @@ class RangeTextFormatter {
     final RegExp exp = RegExp(r'QuranText\[(\([^)]+\)(?:,\([^)]+\))*)\]');
     final Match? match = exp.firstMatch(text);
     if (match != null) {
-      final List<String> ranges = match.group(1)!.split('),');
+      final List<String> ranges = match.group(1)!.split(',');
       final List<VerseRange> parsedRanges = [];
       for (final String range in ranges) {
-        final range0 = range.endsWith(")") ? range : "$range)";
-
-        final RegExp rangeExp = RegExp(r'\((\d+):(\d+),(\d+):(\d+)\)');
-        final Match? rangeMatch = rangeExp.firstMatch(range0);
+        final RegExp rangeExp = RegExp(r'\((\d+):(\d+):(\d+)\)');
+        final Match? rangeMatch = rangeExp.firstMatch(range);
         if (rangeMatch != null) {
-          final int startSura0 = int.parse(rangeMatch.group(1)!);
-          final int startAya0 = int.parse(rangeMatch.group(2)!);
-          final int endSura0 = int.parse(rangeMatch.group(3)!);
-          final int endAya0 = int.parse(rangeMatch.group(4)!);
-
-          ///
-          final int startSura = min(startSura0, endSura0);
-          final int startAya = min(startAya0, endAya0);
-          final int endSura = max(startSura0, endSura0);
-          final int endAya = max(startAya0, endAya0);
-          parsedRanges.add(VerseRange(startSura, startAya, endSura, endAya));
+          final int suraStart = int.parse(rangeMatch.group(1)!);
+          final int ayaStart = int.parse(rangeMatch.group(2)!);
+          final int ayaEnd = int.parse(rangeMatch.group(3)!);
+          parsedRanges.add(VerseRange(suraStart, ayaStart, suraStart, ayaEnd));
         }
       }
-
       return parsedRanges;
     } else {
       hisnPrint("no match");
