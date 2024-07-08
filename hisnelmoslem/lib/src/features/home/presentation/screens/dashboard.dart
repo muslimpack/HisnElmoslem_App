b1558591f4a9d361c053416a4ce622c64eb1b274
7eltantawy <hasaneltantawy@gmail.com>
2024-07-08 22:41:18 +0300

    dashboard add material scaffold

--- hisnelmoslem/lib/src/features/home/presentation/screens/dashboard.dart
+++ hisnelmoslem/lib/src/features/home/presentation/screens/dashboard.dart
@@ -20,22 +20,24 @@ class AzkarDashboard extends StatelessWidget {
       init: DashboardController(),
       builder: (controller) => controller.isLoading
           ? const Loading()
-          : ZoomDrawer(
-              isRtl: Bidi.isRtlLanguage(Get.locale!.languageCode),
-              controller: controller.zoomDrawerController,
-              menuBackgroundColor: Theme.of(context).scaffoldBackgroundColor,
-              menuScreen: ScreenMenu(
-                controller: controller,
+          : Scaffold(
+              body: ZoomDrawer(
+                isRtl: Bidi.isRtlLanguage(Get.locale!.languageCode),
+                controller: controller.zoomDrawerController,
+                menuBackgroundColor: Theme.of(context).scaffoldBackgroundColor,
+                menuScreen: ScreenMenu(
+                  controller: controller,
+                ),
+                mainScreen: MainScreen(
+                  controller: controller,
+                ),
+                borderRadius: 24.0,
+                showShadow: true,
+                angle: 0.0,
+                drawerShadowsBackgroundColor:
+                    Theme.of(context).colorScheme.primary,
+                slideWidth: 270,
               ),
-              mainScreen: MainScreen(
-                controller: controller,
-              ),
-              borderRadius: 24.0,
-              showShadow: true,
-              angle: 0.0,
-              drawerShadowsBackgroundColor:
-                  Theme.of(context).colorScheme.primary,
-              slideWidth: 270,
             ),
     );
   }
