64e2094b8a4133818250f0eb23ee324968857193
7eltantawy <hasaneltantawy@gmail.com>
2024-07-11 19:55:11 +0300

    Update awesome_notification_manager.dart

--- hisnelmoslem/lib/src/features/alarms_manager/data/models/awesome_notification_manager.dart
+++ hisnelmoslem/lib/src/features/alarms_manager/data/models/awesome_notification_manager.dart
@@ -1,4 +1,4 @@
-import 'package:awesome_notifications';
+import 'package:awesome_notifications/awesome_notifications.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:hisnelmoslem/src/core/functions/print.dart';
