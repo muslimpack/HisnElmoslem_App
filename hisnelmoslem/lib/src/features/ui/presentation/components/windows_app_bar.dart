0d090de97982663ddaf1f024237559f7e4dc36e9
7eltantawy <hasaneltantawy@gmail.com>
2024-09-01 03:43:15 +0300

    Add toggle theme icon button to app title bar

--- hisnelmoslem/lib/src/features/ui/presentation/components/windows_app_bar.dart
+++ hisnelmoslem/lib/src/features/ui/presentation/components/windows_app_bar.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:get/get.dart';
 import 'package:hisnelmoslem/src/core/extensions/extension_platform.dart';
+import 'package:hisnelmoslem/src/features/themes/presentation/controller/cubit/theme_cubit.dart';
 import 'package:hisnelmoslem/src/features/ui/presentation/components/windows_button.dart';
 import 'package:window_manager/window_manager.dart';
 
@@ -52,6 +55,26 @@ class _UIAppBarState extends State<UIAppBar> {
                   ),
                 ),
               ),
+            BlocBuilder<ThemeCubit, ThemeState>(
+              builder: (context, state) {
+                return Align(
+                  alignment: AlignmentDirectional.centerEnd,
+                  child: Padding(
+                    padding: const EdgeInsetsDirectional.only(end: 8.0),
+                    child: ChoiceChip(
+                      selected: state.brightness == Brightness.dark,
+                      showCheckmark: false,
+                      label: const Icon(FontAwesomeIcons.solidMoon),
+                      onSelected: (v) {
+                        context.read<ThemeCubit>().changeBrightness(
+                              v ? Brightness.dark : Brightness.light,
+                            );
+                      },
+                    ),
+                  ),
+                );
+              },
+            ),
             if (!kIsWeb) const WindowButtons(),
           ],
         ),
