9458e2d100eed659416b5b88d1175ff53b542ae5
7eltantawy <hasaneltantawy@gmail.com>
2024-09-03 02:11:40 +0300

    start event

--- hisnelmoslem/lib/src/features/tally/presentation/controller/bloc/tally_bloc.dart
+++ hisnelmoslem/lib/src/features/tally/presentation/controller/bloc/tally_bloc.dart
@@ -2,14 +2,31 @@ import 'dart:async';
 
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter/services.dart';
 import 'package:hisnelmoslem/src/features/tally/data/models/tally.dart';
 import 'package:hisnelmoslem/src/features/tally/data/models/tally_iteration_mode.dart';
+import 'package:hisnelmoslem/src/features/tally/data/repository/tally_database_helper.dart';
 
 part 'tally_event.dart';
 part 'tally_state.dart';
 
 class TallyBloc extends Bloc<TallyEvent, TallyState> {
+  final _volumeBtnChannel = const MethodChannel("volume_button_channel");
   TallyBloc() : super(TallyLoadingState()) {
+    _initHandlers();
+
+    _volumeBtnChannel.setMethodCallHandler((call) async {
+      if (call.method == "volumeBtnPressed") {
+        if (call.arguments == "VOLUME_DOWN_UP") {
+          add(TallyIncreaseActiveCounterEvent());
+        } else if (call.arguments == "VOLUME_UP_UP") {
+          add(TallyIncreaseActiveCounterEvent());
+        }
+      }
+    });
+  }
+
+  void _initHandlers() {
     on<TallyStartEvent>(_start);
     on<TallyAddCounterEvent>(_addCounter);
     on<TallyEditCounterEvent>(_editCounter);
@@ -26,55 +43,71 @@ class TallyBloc extends Bloc<TallyEvent, TallyState> {
   FutureOr<void> _start(
     TallyStartEvent event,
     Emitter<TallyState> emit,
-  ) {}
+  ) async {
+    final allCounters = await tallyDatabaseHelper.getAllTally();
+
+    emit(
+      TallyLoadedState(
+        allCounters: allCounters,
+        activeCounter: allCounters.firstOrNull,
+        iterationMode: TallyIterationMode.none,
+      ),
+    );
+  }
 
   FutureOr<void> _addCounter(
     TallyAddCounterEvent event,
     Emitter<TallyState> emit,
-  ) {}
+  ) async {}
 
   FutureOr<void> _editCounter(
     TallyEditCounterEvent event,
     Emitter<TallyState> emit,
-  ) {}
+  ) async {}
 
   FutureOr<void> _deleteCounter(
     TallyDeleteCounterEvent event,
     Emitter<TallyState> emit,
-  ) {}
+  ) async {}
 
   FutureOr<void> _activateCounter(
     TallyActivateCounterEvent event,
     Emitter<TallyState> emit,
-  ) {}
+  ) async {}
 
   FutureOr<void> _nextCounter(
     TallyNextCounterEvent event,
     Emitter<TallyState> emit,
-  ) {}
+  ) async {}
 
   FutureOr<void> _previousCounter(
     TallyPreviousCounterEvent event,
     Emitter<TallyState> emit,
-  ) {}
+  ) async {}
 
   FutureOr<void> _resetAllCounters(
     TallyResetAllCountersEvent event,
     Emitter<TallyState> emit,
-  ) {}
+  ) async {}
 
   FutureOr<void> _resetActiveCounter(
     TallyResetActiveCounterEvent event,
     Emitter<TallyState> emit,
-  ) {}
+  ) async {}
 
   FutureOr<void> _increaseActiveCounter(
     TallyIncreaseActiveCounterEvent event,
     Emitter<TallyState> emit,
-  ) {}
+  ) async {}
 
   FutureOr<void> _decreaseActiveCounter(
     TallyDecreaseActiveCounterEvent event,
     Emitter<TallyState> emit,
-  ) {}
+  ) async {}
+
+  @override
+  Future<void> close() async {
+    _volumeBtnChannel.setMethodCallHandler(null);
+    return super.close();
+  }
 }
