b4f09def082945ef697ac103c90eed914e1bbeac
7eltantawy <hasaneltantawy@gmail.com>
2024-09-04 02:53:13 +0300

    Update widget_capture_controller.dart

--- hisnelmoslem/packages/capture_widget/lib/core/widget_capture_controller.dart
+++ hisnelmoslem/packages/capture_widget/lib/core/widget_capture_controller.dart
@@ -16,16 +16,29 @@ class CaptureWidgetController {
   /// Returns a [Future] that completes with the captured image as a [ui.Image]
   /// object, or `null` if the widget or its context is not available.
   Future<ui.Image?> getImage(double pixelRatio) async {
-    final context = _imageKey.currentContext;
-    if (context == null) return null;
-
-    final RenderRepaintBoundary? boundary =
-        context.findRenderObject() as RenderRepaintBoundary?;
+    final RenderRepaintBoundary? boundary = _getBoundary();
 
     if (boundary == null) return null;
 
     final image = await boundary.toImage(pixelRatio: pixelRatio);
 
     return image;
+  }
+
+  /// Retrieves the size of the widget.
+  ///
+  /// Returns the size of the widget as a [Size] object, or `null` if the widget or its context is not available.
+  Size? getWidgetSize() {
+    return _getBoundary()?.size;
+  }
+
+  RenderRepaintBoundary? _getBoundary() {
+    final context = _imageKey.currentContext;
+    if (context == null) return null;
+
+    final RenderRepaintBoundary? boundary =
+        context.findRenderObject() as RenderRepaintBoundary?;
+
+    return boundary;
   }
 }
