2d5f8774c070a963c58138a5ec69aa56d8c6b205
7eltantawy <hasaneltantawy@gmail.com>
2024-09-03 23:51:21 +0300

    Update quran_read_page.dart

--- hisnelmoslem/lib/src/features/quran/presentation/screens/quran_read_page.dart
+++ hisnelmoslem/lib/src/features/quran/presentation/screens/quran_read_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:get/get.dart';
 import 'package:hisnelmoslem/src/core/extensions/extension_object.dart';
 import 'package:hisnelmoslem/src/core/shared/widgets/loading.dart';
@@ -6,7 +7,7 @@ import 'package:hisnelmoslem/src/core/values/constant.dart';
 import 'package:hisnelmoslem/src/features/quran/data/models/surah_name_enum.dart';
 import 'package:hisnelmoslem/src/features/quran/presentation/components/between_page_effect.dart';
 import 'package:hisnelmoslem/src/features/quran/presentation/components/page_side_effect.dart';
-import 'package:hisnelmoslem/src/features/quran/presentation/controller/quran_controller.dart';
+import 'package:hisnelmoslem/src/features/quran/presentation/controller/cubit/quran_cubit.dart';
 
 class QuranReadPage extends StatelessWidget {
   final SurahNameEnum surahName;
@@ -15,83 +16,80 @@ class QuranReadPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return GetBuilder<QuranPageController>(
-      init: QuranPageController(surahName: surahName),
-      builder: (controller) {
-        return controller.isLoading
-            ? const Loading()
-            : Scaffold(
-                key: controller.quranReadPageScaffoldKey,
-                appBar: AppBar(
-                  elevation: 0,
-                  centerTitle: true,
-                  title: Text(
-                    () {
-                      switch (controller.surahName) {
-                        case SurahNameEnum.alKahf:
-                          return 'sura Al-Kahf'.tr;
-                        case SurahNameEnum.alMulk:
-                          return 'sura Al-Mulk'.tr;
-                        case SurahNameEnum.assajdah:
-                          return 'sura As-Sajdah'.tr;
-                        case SurahNameEnum.endofAliImran:
-                          return "end sura Ali 'Imran".tr;
-                      }
-                    }(),
-                  ),
-                ),
-                body: GestureDetector(
-                  onDoubleTap: () => controller.onDoubleTap(),
-                  child: PageView.builder(
-                    physics: const BouncingScrollPhysics(),
-                    onPageChanged: controller.onPageViewChange,
-                    controller: controller.pageController,
-                    itemCount: controller.quranRequiredSurah!.pages.length,
-                    itemBuilder: (context, index) {
-                      return Stack(
-                        children: [
-                          BetweenPageEffect(
-                            index: controller
-                                .quranRequiredSurah!.pages[index].pageNumber,
-                          ),
-                          PageSideEffect(
-                            index: controller
-                                .quranRequiredSurah!.pages[index].pageNumber,
-                          ),
-                          Center(
-                            child: ColorFiltered(
-                              colorFilter: greyScale,
-                              child: ColorFiltered(
-                                colorFilter: Theme.of(context).brightness ==
-                                        Brightness.dark
+    return BlocProvider(
+      create: (context) => QuranCubit()..start(surahName),
+      child: BlocBuilder<QuranCubit, QuranState>(
+        builder: (context, state) {
+          if (state is! QuranLoadedState) {
+            return const Loading();
+          }
+          return Scaffold(
+            appBar: AppBar(
+              elevation: 0,
+              centerTitle: true,
+              title: Text(
+                () {
+                  switch (state.surahName) {
+                    case SurahNameEnum.alKahf:
+                      return 'sura Al-Kahf'.tr;
+                    case SurahNameEnum.alMulk:
+                      return 'sura Al-Mulk'.tr;
+                    case SurahNameEnum.assajdah:
+                      return 'sura As-Sajdah'.tr;
+                    case SurahNameEnum.endofAliImran:
+                      return "end sura Ali 'Imran".tr;
+                  }
+                }(),
+              ),
+            ),
+            body: GestureDetector(
+              onDoubleTap: () => context.read<QuranCubit>().onDoubleTap(),
+              child: PageView.builder(
+                physics: const BouncingScrollPhysics(),
+                controller: context.read<QuranCubit>().pageController,
+                itemCount: state.requiredSurah.pages.length,
+                itemBuilder: (context, index) {
+                  return Stack(
+                    children: [
+                      BetweenPageEffect(
+                        index: state.requiredSurah.pages[index].pageNumber,
+                      ),
+                      PageSideEffect(
+                        index: state.requiredSurah.pages[index].pageNumber,
+                      ),
+                      Center(
+                        child: ColorFiltered(
+                          colorFilter: greyScale,
+                          child: ColorFiltered(
+                            colorFilter:
+                                Theme.of(context).brightness == Brightness.dark
                                     ? invert
                                     : normal,
-                                child: Image.asset(
-                                  controller
-                                      .quranRequiredSurah!.pages[index].image,
-                                  fit: BoxFit.fitWidth,
-                                ),
-                              ),
+                            child: Image.asset(
+                              state.requiredSurah.pages[index].image,
+                              fit: BoxFit.fitWidth,
                             ),
                           ),
-                          Align(
-                            alignment: Alignment.bottomCenter,
-                            child: Padding(
-                              padding: const EdgeInsets.all(10),
-                              child: Text(
-                                controller
-                                    .quranRequiredSurah!.pages[index].pageNumber
-                                    .toArabicNumberString(),
-                              ),
-                            ),
+                        ),
+                      ),
+                      Align(
+                        alignment: Alignment.bottomCenter,
+                        child: Padding(
+                          padding: const EdgeInsets.all(10),
+                          child: Text(
+                            state.requiredSurah.pages[index].pageNumber
+                                .toArabicNumberString(),
                           ),
-                        ],
-                      );
-                    },
-                  ),
-                ),
-              );
-      },
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+          );
+        },
+      ),
     );
   }
 }
