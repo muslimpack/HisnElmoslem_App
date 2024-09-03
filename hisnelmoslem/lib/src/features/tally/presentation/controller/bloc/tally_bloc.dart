0cda1a3c0aedf37850762f2b669b12de8f389fb8
7eltantawy <hasaneltantawy@gmail.com>
2024-09-03 03:29:48 +0300

    Update tally_bloc.dart

--- hisnelmoslem/lib/src/features/tally/presentation/controller/bloc/tally_bloc.dart
+++ hisnelmoslem/lib/src/features/tally/presentation/controller/bloc/tally_bloc.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'dart:async';
 
 import 'package:bloc/bloc.dart';
@@ -11,8 +12,11 @@ part 'tally_event.dart';
 part 'tally_state.dart';
 
 class TallyBloc extends Bloc<TallyEvent, TallyState> {
+  final TallyDatabaseHelper tallyDatabaseHelper;
   final _volumeBtnChannel = const MethodChannel("volume_button_channel");
-  TallyBloc() : super(TallyLoadingState()) {
+  TallyBloc(
+    this.tallyDatabaseHelper,
+  ) : super(TallyLoadingState()) {
     _initHandlers();
 
     _volumeBtnChannel.setMethodCallHandler((call) async {
