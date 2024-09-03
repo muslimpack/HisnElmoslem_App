76871578015ba593d9a11e382d1ce0f16e8c404a
7eltantawy <hasaneltantawy@gmail.com>
2024-09-03 06:48:12 +0300

    Update tally_list.dart

--- hisnelmoslem/lib/src/features/tally/presentation/components/pages/tally_list.dart
+++ hisnelmoslem/lib/src/features/tally/presentation/components/pages/tally_list.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:get/get.dart';
+import 'package:hisnelmoslem/src/core/shared/dialogs/yes_no_dialog.dart';
 import 'package:hisnelmoslem/src/core/shared/widgets/loading.dart';
 import 'package:hisnelmoslem/src/features/tally/data/models/tally.dart';
 import 'package:hisnelmoslem/src/features/tally/presentation/components/dialogs/tally_dialog.dart';
@@ -59,7 +61,20 @@ class TallyListView extends StatelessWidget {
                   size: 40,
                 ),
                 onPressed: () {
-                  context.read<TallyBloc>().add(TallyResetAllCountersEvent());
+                  showModalBottomSheet(
+                    isScrollControlled: true,
+                    context: context,
+                    builder: (_) {
+                      return YesOrNoDialog(
+                        msg: "Reset all counters?.".tr,
+                        onYes: () async {
+                          context
+                              .read<TallyBloc>()
+                              .add(TallyResetAllCountersEvent());
+                        },
+                      );
+                    },
+                  );
                 },
               ),
             ],
