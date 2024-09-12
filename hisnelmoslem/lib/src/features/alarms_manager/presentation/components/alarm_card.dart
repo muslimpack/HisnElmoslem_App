8cdf21d3372a985addfd8f8b3afb465e2e1b3d6a
7eltantawy <hasaneltantawy@gmail.com>
2024-09-13 01:01:36 +0300

    Impl AlarmsBloc

--- hisnelmoslem/lib/src/features/alarms_manager/presentation/components/alarm_card.dart
+++ hisnelmoslem/lib/src/features/alarms_manager/presentation/components/alarm_card.dart
@@ -1,90 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:get/get.dart';
-import 'package:hisnelmoslem/src/core/functions/get_snackbar.dart';
 import 'package:hisnelmoslem/src/core/functions/handle_repeat_type.dart';
 import 'package:hisnelmoslem/src/core/shared/dialogs/alarm_dialog.dart';
 import 'package:hisnelmoslem/src/core/shared/widgets/round_tag.dart';
 import 'package:hisnelmoslem/src/features/alarms_manager/data/models/alarm.dart';
-import 'package:hisnelmoslem/src/features/alarms_manager/data/models/alarm_manager.dart';
-import 'package:hisnelmoslem/src/features/alarms_manager/data/models/awesome_notification_manager.dart';
-import 'package:hisnelmoslem/src/features/alarms_manager/data/repository/alarm_database_helper.dart';
 import 'package:hisnelmoslem/src/features/alarms_manager/presentation/controller/alarm_controller.dart';
-import 'package:hisnelmoslem/src/features/home/presentation/controller/dashboard_controller.dart';
+import 'package:hisnelmoslem/src/features/alarms_manager/presentation/controller/bloc/alarms_bloc.dart';
 
 class AlarmCard extends StatelessWidget {
   final DbAlarm dbAlarm;
 
-  AlarmCard({super.key, required this.dbAlarm});
+  const AlarmCard({super.key, required this.dbAlarm});
 
-  //
-  final DashboardController dashboardController =
-      Get.put(DashboardController());
-
-  Widget alarmCardBody() {
-    return GetBuilder<AlarmsPageController>(
-      builder: (controller) {
-        return Column(
-          children: [
-            SwitchListTile(
-              title: ListTile(
-                contentPadding: EdgeInsets.zero,
-                leading: const Icon(Icons.alarm),
-                subtitle: Wrap(
-                  children: [
-                    if (dbAlarm.body.isEmpty)
-                      const SizedBox()
-                    else
-                      RoundTagCard(
-                        name: dbAlarm.body,
-                        color: Colors.brown,
-                      ),
-                    Row(
-                      children: [
-                        Expanded(
-                          child: RoundTagCard(
-                            name: '⌚ ${dbAlarm.hour} : ${dbAlarm.minute}',
-                            color: Colors.green,
-                          ),
-                        ),
-                        Expanded(
-                          child: RoundTagCard(
-                            name: HandleRepeatType()
-                                .getNameToUser(chosenValue: dbAlarm.repeatType),
-                            color: Colors.yellow,
-                          ),
-                        ),
-                      ],
-                    ),
-                  ],
-                ),
-                isThreeLine: true,
-                title: Text(dbAlarm.title),
-              ),
-              value: dbAlarm.isActive,
-              onChanged: (value) {
-                //Update database
-                final DbAlarm updateAlarm = dbAlarm;
-                updateAlarm.isActive = value;
-                alarmDatabaseHelper.updateAlarmInfo(dbAlarm: updateAlarm);
-                // update view
-                dbAlarm.isActive = value;
-                //
-                alarmManager.alarmState(dbAlarm: updateAlarm);
-                //
-                controller.update();
-                dashboardController.alarms = controller.alarms;
-                dashboardController.update();
-              },
-            ),
-            // Divider(),
-          ],
-        );
-      },
-    );
-  }
-
-//
   @override
   Widget build(BuildContext context) {
     return GetBuilder<AlarmsPageController>(
@@ -95,24 +24,18 @@ class AlarmCard extends StatelessWidget {
             motion: const BehindMotion(),
             children: [
               SlidableAction(
-                onPressed: (val) {
-                  showFastAlarmDialog(
+                onPressed: (val) async {
+                  final alarm = await showFastAlarmDialog(
                     context: context,
                     dbAlarm: dbAlarm,
                     isToEdit: true,
-                  ).then((value) {
-                    if (value is DbAlarm) {
-                      if (value.hasAlarmInside) {
-                        final int index = controller.alarms.indexOf(dbAlarm);
-                        controller.alarms[index] = value;
-                        //
-                        dashboardController.alarms = controller.alarms;
-                        dashboardController.update();
-                        //
-                        controller.update();
-                      }
-                    }
-                  });
+                  );
+
+                  if (alarm is! DbAlarm) return;
+                  if (!alarm.hasAlarmInside) return;
+                  if (!context.mounted) return;
+
+                  context.read<AlarmsBloc>().add(AlarmsEditEvent(alarm));
                 },
                 icon: Icons.edit,
                 label: 'edit'.tr,
@@ -125,27 +48,79 @@ class AlarmCard extends StatelessWidget {
             children: [
               SlidableAction(
                 onPressed: (val) async {
-                  await awesomeNotificationManager.cancelNotificationById(
-                    id: dbAlarm.titleId,
-                  );
-                  await alarmDatabaseHelper.deleteAlarm(dbAlarm: dbAlarm);
-                  controller.alarms.removeWhere((item) => item == dbAlarm);
-                  dashboardController.alarms = controller.alarms;
-
-                  controller.update();
-                  dashboardController.update();
-                  getSnackbar(
-                    message: "${"Reminder Removed".tr} | ${dbAlarm.title}",
-                  );
+                  context.read<AlarmsBloc>().add(AlarmsRemoveEvent(dbAlarm));
                 },
                 icon: Icons.delete,
                 label: "delete".tr,
               ),
             ],
           ),
-          child: alarmCardBody(),
+          child: AlarmCardBody(dbAlarm: dbAlarm, context: context),
         );
       },
+    );
+  }
+}
+
+class AlarmCardBody extends StatelessWidget {
+  const AlarmCardBody({
+    super.key,
+    required this.dbAlarm,
+    required this.context,
+  });
+
+  final DbAlarm dbAlarm;
+  final BuildContext context;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        SwitchListTile(
+          title: ListTile(
+            contentPadding: EdgeInsets.zero,
+            leading: const Icon(Icons.alarm),
+            subtitle: Wrap(
+              children: [
+                if (dbAlarm.body.isEmpty)
+                  const SizedBox()
+                else
+                  RoundTagCard(
+                    name: dbAlarm.body,
+                    color: Colors.brown,
+                  ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: RoundTagCard(
+                        name: '⌚ ${dbAlarm.hour} : ${dbAlarm.minute}',
+                        color: Colors.green,
+                      ),
+                    ),
+                    Expanded(
+                      child: RoundTagCard(
+                        name: HandleRepeatType()
+                            .getNameToUser(chosenValue: dbAlarm.repeatType),
+                        color: Colors.yellow,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+            isThreeLine: true,
+            title: Text(dbAlarm.title),
+          ),
+          value: dbAlarm.isActive,
+          onChanged: (value) {
+            context.read<AlarmsBloc>().add(
+                  AlarmsEditEvent(
+                    dbAlarm.copyWith(isActive: value),
+                  ),
+                );
+          },
+        ),
+      ],
     );
   }
 }
