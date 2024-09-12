8295fe099f9b9b791719454a945996dbcbba57de
7eltantawy <hasaneltantawy@gmail.com>
2024-09-12 20:06:15 +0300

    Update azkar_read_page_controller.dart

--- hisnelmoslem/lib/src/features/zikr_viewer/presentation/controller/azkar_read_page_controller.dart
+++ hisnelmoslem/lib/src/features/zikr_viewer/presentation/controller/azkar_read_page_controller.dart
@@ -1,175 +1,175 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:get/get.dart';
-import 'package:hisnelmoslem/src/core/functions/get_snackbar.dart';
-import 'package:hisnelmoslem/src/core/repos/app_data.dart';
-import 'package:hisnelmoslem/src/features/effects_manager/presentation/controller/sounds_manager_controller.dart';
-import 'package:hisnelmoslem/src/features/home/data/models/zikr_title.dart';
-import 'package:hisnelmoslem/src/features/home/data/repository/azkar_database_helper.dart';
-import 'package:hisnelmoslem/src/features/zikr_viewer/data/models/zikr_content.dart';
-import 'package:wakelock_plus/wakelock_plus.dart';
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:get/get.dart';
+// import 'package:hisnelmoslem/src/core/functions/get_snackbar.dart';
+// import 'package:hisnelmoslem/src/core/repos/app_data.dart';
+// import 'package:hisnelmoslem/src/features/effects_manager/presentation/controller/sounds_manager_controller.dart';
+// import 'package:hisnelmoslem/src/features/home/data/models/zikr_title.dart';
+// import 'package:hisnelmoslem/src/features/home/data/repository/azkar_database_helper.dart';
+// import 'package:hisnelmoslem/src/features/zikr_viewer/data/models/zikr_content.dart';
+// import 'package:wakelock_plus/wakelock_plus.dart';
 
-class AzkarReadPageController extends GetxController {
-  /* *************** Constructor *************** */
-  //
-  final int index;
+// class AzkarReadPageController extends GetxController {
+//   /* *************** Constructor *************** */
+//   //
+//   final int index;
 
-  AzkarReadPageController({required this.index});
+//   AzkarReadPageController({required this.index});
 
-  /* *************** Variables *************** */
-  //
-  bool isLoading = true;
+//   /* *************** Variables *************** */
+//   //
+//   bool isLoading = true;
 
-  //
-  //
-  final hReadScaffoldKey = GlobalKey<ScaffoldState>();
-  PageController pageController = PageController();
+//   //
+//   //
+//   final hReadScaffoldKey = GlobalKey<ScaffoldState>();
+//   PageController pageController = PageController();
 
-  //
-  List<DbContent> zikrContent = <DbContent>[];
-  DbTitle? zikrTitle;
+//   //
+//   List<DbContent> zikrContent = <DbContent>[];
+//   DbTitle? zikrTitle;
 
-  //
-  int currentPage = 0;
-  double? totalProgress = 0.0;
-  int zikrCountSum = 0;
-  double? totalProgressForEverySingle = 0.0;
+//   //
+//   int currentPage = 0;
+//   double? totalProgress = 0.0;
+//   int zikrCountSum = 0;
+//   double? totalProgressForEverySingle = 0.0;
 
-  //
-  static const _volumeBtnChannel = MethodChannel("volume_button_channel");
+//   //
+//   static const _volumeBtnChannel = MethodChannel("volume_button_channel");
 
-  int? cardNum = 0;
+//   int? cardNum = 0;
 
-  DbContent get activeZikr => zikrContent[currentPage];
+//   DbContent get activeZikr => zikrContent[currentPage];
 
-  //
+//   //
 
-  /* *************** Controller life cycle *************** */
-  //
-  @override
-  Future<void> onInit() async {
-    super.onInit();
-    //
-    if (AppData.instance.enableWakeLock) {
-      WakelockPlus.enable();
-    }
+//   /* *************** Controller life cycle *************** */
+//   //
+//   @override
+//   Future<void> onInit() async {
+//     super.onInit();
+//     //
+//     if (AppData.instance.enableWakeLock) {
+//       WakelockPlus.enable();
+//     }
 
-    ///
-    _volumeBtnChannel.setMethodCallHandler((call) {
-      if (call.method == "volumeBtnPressed") {
-        if (call.arguments == "VOLUME_DOWN_UP" ||
-            call.arguments == "VOLUME_UP_UP") {
-          decreaseCount();
-        }
-        if (call.arguments == "MEDIA_BUTTON_DOWN") {
-          getSnackbar(message: "MEDIA_BUTTON_DOWN");
-        }
-      }
+//     ///
+//     _volumeBtnChannel.setMethodCallHandler((call) {
+//       if (call.method == "volumeBtnPressed") {
+//         if (call.arguments == "VOLUME_DOWN_UP" ||
+//             call.arguments == "VOLUME_UP_UP") {
+//           decreaseCount();
+//         }
+//         if (call.arguments == "MEDIA_BUTTON_DOWN") {
+//           getSnackbar(message: "MEDIA_BUTTON_DOWN");
+//         }
+//       }
 
-      return Future.value();
-    });
+//       return Future.value();
+//     });
 
-    await getReady();
-    getEveryZikrCount();
-    //
-    isLoading = false;
-    //
-    update();
-  }
+//     await getReady();
+//     getEveryZikrCount();
+//     //
+//     isLoading = false;
+//     //
+//     update();
+//   }
 
-  //
-  @override
-  void onClose() {
-    super.onClose();
-    WakelockPlus.disable();
-    pageController.dispose();
-    _volumeBtnChannel.setMethodCallHandler(null);
-  }
+//   //
+//   @override
+//   void onClose() {
+//     super.onClose();
+//     WakelockPlus.disable();
+//     pageController.dispose();
+//     _volumeBtnChannel.setMethodCallHandler(null);
+//   }
 
-  /* *************** Functions *************** */
-  //
+//   /* *************** Functions *************** */
+//   //
 
-  Future<void> getReady() async {
-    await azkarDatabaseHelper
-        .getTitleById(id: index)
-        .then((value) => zikrTitle = value);
-    //
-    await azkarDatabaseHelper
-        .getContentsByTitleId(titleId: index)
-        .then((value) => zikrContent = value);
+//   Future<void> getReady() async {
+//     await azkarDatabaseHelper
+//         .getTitleById(id: index)
+//         .then((value) => zikrTitle = value);
+//     //
+//     await azkarDatabaseHelper
+//         .getContentsByTitleId(titleId: index)
+//         .then((value) => zikrContent = value);
 
-    isLoading = false;
-    update();
-  }
+//     isLoading = false;
+//     update();
+//   }
 
-  void onPageViewChange(int page) {
-    currentPage = page;
-    update();
-  }
+//   void onPageViewChange(int page) {
+//     currentPage = page;
+//     update();
+//   }
 
-  void decreaseCount() {
-    int counter = zikrContent[currentPage].count;
+//   void decreaseCount() {
+//     int counter = zikrContent[currentPage].count;
 
-    if (counter > 0) {
-      counter--;
+//     if (counter > 0) {
+//       counter--;
 
-      zikrContent[currentPage].count = (zikrContent[currentPage].count) - 1;
+//       zikrContent[currentPage].count = (zikrContent[currentPage].count) - 1;
 
-      SoundsManagerController().playTallyEffects();
-      if (counter == 0) {
-        SoundsManagerController().playZikrDoneEffects();
-        SoundsManagerController().playTransitionEffects();
-      }
-    }
+//       SoundsManagerController().playTallyEffects();
+//       if (counter == 0) {
+//         SoundsManagerController().playZikrDoneEffects();
+//         SoundsManagerController().playTransitionEffects();
+//       }
+//     }
 
-    if (counter == 0) {
-      pageController.nextPage(
-        curve: Curves.easeIn,
-        duration: const Duration(milliseconds: 350),
-      );
-    }
+//     if (counter == 0) {
+//       pageController.nextPage(
+//         curve: Curves.easeIn,
+//         duration: const Duration(milliseconds: 350),
+//       );
+//     }
 
-    ///
-    checkProgress();
-    checkProgressForSingle();
+//     ///
+//     checkProgress();
+//     checkProgressForSingle();
 
-    ///
-    update();
-  }
+//     ///
+//     update();
+//   }
 
-  void checkProgress() {
-    int totalNum = 0;
-    int done = 0;
-    totalNum = zikrContent.length;
-    for (var i = 0; i < zikrContent.length; i++) {
-      if (zikrContent[i].count == 0) {
-        done++;
-      }
-    }
-    totalProgress = done / totalNum;
-    if (totalProgress == 1) {
-      ///
-      SoundsManagerController().playAllAzkarFinishedEffects();
-    }
-    update();
-  }
+//   void checkProgress() {
+//     int totalNum = 0;
+//     int done = 0;
+//     totalNum = zikrContent.length;
+//     for (var i = 0; i < zikrContent.length; i++) {
+//       if (zikrContent[i].count == 0) {
+//         done++;
+//       }
+//     }
+//     totalProgress = done / totalNum;
+//     if (totalProgress == 1) {
+//       ///
+//       SoundsManagerController().playAllAzkarFinishedEffects();
+//     }
+//     update();
+//   }
 
-  void checkProgressForSingle() {
-    int done = 0;
-    for (var i = 0; i < zikrContent.length; i++) {
-      done += zikrContent[i].count;
-    }
-    totalProgressForEverySingle = (zikrCountSum - done) / zikrCountSum;
-    update();
-  }
+//   void checkProgressForSingle() {
+//     int done = 0;
+//     for (var i = 0; i < zikrContent.length; i++) {
+//       done += zikrContent[i].count;
+//     }
+//     totalProgressForEverySingle = (zikrCountSum - done) / zikrCountSum;
+//     update();
+//   }
 
-  void getEveryZikrCount() {
-    int sum = 0;
+//   void getEveryZikrCount() {
+//     int sum = 0;
 
-    for (var i = 0; i < zikrContent.length; i++) {
-      sum += zikrContent[i].count;
-    }
-    zikrCountSum = sum;
-    update();
-  }
-}
+//     for (var i = 0; i < zikrContent.length; i++) {
+//       sum += zikrContent[i].count;
+//     }
+//     zikrCountSum = sum;
+//     update();
+//   }
+// }
