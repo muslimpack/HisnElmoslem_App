ad8f37bfa78baa1f8f0f5b6cadac30d8d0c58fb2
7eltantawy <hasaneltantawy@gmail.com>
2024-09-14 12:02:01 +0300

    Update alarm_repeat_type.dart

--- hisnelmoslem/lib/src/features/alarms_manager/data/models/alarm_repeat_type.dart
+++ hisnelmoslem/lib/src/features/alarms_manager/data/models/alarm_repeat_type.dart
@@ -1,3 +1,5 @@
+import 'package:get/get.dart';
+
 enum AlarmRepeatType {
   daily,
   atSaturday,
@@ -7,4 +9,27 @@ enum AlarmRepeatType {
   atWednesday,
   atThursday,
   atFriday,
+}
+
+extension AlarmRepeatTypeExtension on AlarmRepeatType {
+  String getUserFriendlyName() {
+    switch (this) {
+      case AlarmRepeatType.daily:
+        return "daily".tr;
+      case AlarmRepeatType.atSaturday:
+        return "every saturday".tr;
+      case AlarmRepeatType.atSunday:
+        return "every sunday".tr;
+      case AlarmRepeatType.atMonday:
+        return "every monday".tr;
+      case AlarmRepeatType.atTuesday:
+        return "every tuesday".tr;
+      case AlarmRepeatType.atWednesday:
+        return "every wednesday".tr;
+      case AlarmRepeatType.atThursday:
+        return "every thursday".tr;
+      case AlarmRepeatType.atFriday:
+        return "every Friday".tr;
+    }
+  }
 }
