a5c67599cdf608b90aac246edc8e2313c3997c6b
7eltantawy <hasaneltantawy@gmail.com>
2024-09-14 14:34:44 +0300

    Update alarm.dart

--- hisnelmoslem/lib/src/features/alarms_manager/data/models/alarm.dart
+++ hisnelmoslem/lib/src/features/alarms_manager/data/models/alarm.dart
@@ -1,8 +1,6 @@
-// ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:equatable/equatable.dart';
 import 'package:hisnelmoslem/src/features/alarms_manager/data/models/alarm_repeat_type.dart';
 
-/// TODO make final fields after removeing GetX
 class DbAlarm extends Equatable {
   final int id;
   final int titleId;
