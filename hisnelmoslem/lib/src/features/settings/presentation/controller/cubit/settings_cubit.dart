84ba20d05180f9b9ad6cdad6fd3aa35db9a8ecd3
7eltantawy <hasaneltantawy@gmail.com>
2024-09-16 14:58:01 +0300

    Settings Cubit add ZikrEffects settings

--- hisnelmoslem/lib/src/features/settings/presentation/controller/cubit/settings_cubit.dart
+++ hisnelmoslem/lib/src/features/settings/presentation/controller/cubit/settings_cubit.dart
@@ -1,8 +1,91 @@
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:hisnelmoslem/src/core/repos/app_data.dart';
+import 'package:hisnelmoslem/src/features/effects_manager/data/models/zikr_effects.dart';
 
 part 'settings_state.dart';
 
 class SettingsCubit extends Cubit<SettingsState> {
-  SettingsCubit() : super(const SettingsState());
+  SettingsCubit()
+      : super(
+          SettingsState(
+            zikrEffects: ZikrEffects(
+              soundEffectVolume: AppData.instance.soundEffectVolume,
+              isTallySoundAllowed: AppData.instance.isTallySoundAllowed,
+              soundEveryPraise: AppData.instance.isZikrDoneSoundAllowed,
+              soundEveryZikr: AppData.instance.isTransitionSoundAllowed,
+              soundEveryTitle: AppData.instance.isAllAzkarFinishedSoundAllowed,
+              isTallyVibrateAllowed: AppData.instance.isTallyVibrateAllowed,
+              vibrateEveryPraise: AppData.instance.isZikrDoneVibrateAllowed,
+              vibrateEveryZikr: AppData.instance.isTransitionVibrateAllowed,
+              vibrateEveryTitle:
+                  AppData.instance.isAllAzkarFinishedVibrateAllowed,
+            ),
+          ),
+        );
+
+  ///MARK: Zikr Effects
+  Future zikrEffectChangeVolume(double value) async {
+    emit(
+      state.copyWith(
+        zikrEffects: state.zikrEffects.copyWith(soundEffectVolume: value),
+      ),
+    );
+  }
+
+  Future zikrEffectChangePlaySoundEveryPraise({required bool activate}) async {
+    emit(
+      state.copyWith(
+        zikrEffects: state.zikrEffects.copyWith(soundEveryPraise: activate),
+      ),
+    );
+  }
+
+  Future zikrEffectChangePlaySoundEveryZikr({required bool activate}) async {
+    emit(
+      state.copyWith(
+        zikrEffects: state.zikrEffects.copyWith(soundEveryZikr: activate),
+      ),
+    );
+  }
+
+  Future zikrEffectChangePlaySoundEveryTitle({required bool activate}) async {
+    emit(
+      state.copyWith(
+        zikrEffects: state.zikrEffects.copyWith(soundEveryTitle: activate),
+      ),
+    );
+  }
+
+  /* Vibration */
+
+  Future zikrEffectChangePlayVibrationEveryPraise({
+    required bool activate,
+  }) async {
+    emit(
+      state.copyWith(
+        zikrEffects: state.zikrEffects.copyWith(vibrateEveryPraise: activate),
+      ),
+    );
+  }
+
+  Future zikrEffectChangePlayVibrationEveryZikr({
+    required bool activate,
+  }) async {
+    emit(
+      state.copyWith(
+        zikrEffects: state.zikrEffects.copyWith(vibrateEveryZikr: activate),
+      ),
+    );
+  }
+
+  Future zikrEffectChangePlayVibrationEveryTitle({
+    required bool activate,
+  }) async {
+    emit(
+      state.copyWith(
+        zikrEffects: state.zikrEffects.copyWith(vibrateEveryTitle: activate),
+      ),
+    );
+  }
 }
--- hisnelmoslem/lib/src/features/settings/presentation/controller/cubit/settings_state.dart
+++ hisnelmoslem/lib/src/features/settings/presentation/controller/cubit/settings_state.dart
@@ -1,8 +1,20 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
 part of 'settings_cubit.dart';
 
 class SettingsState extends Equatable {
-  const SettingsState();
+  final ZikrEffects zikrEffects;
+  const SettingsState({
+    required this.zikrEffects,
+  });
 
   @override
-  List<Object> get props => [];
+  List<Object> get props => [zikrEffects];
+
+  SettingsState copyWith({
+    ZikrEffects? zikrEffects,
+  }) {
+    return SettingsState(
+      zikrEffects: zikrEffects ?? this.zikrEffects,
+    );
+  }
 }
