a937d8e2ff4815c4253a3292d6b143dd4474ab9c
7eltantawy <hasaneltantawy@gmail.com>
2024-09-16 14:59:26 +0300

    Update sounds_manager_controller.dart

--- hisnelmoslem/lib/src/features/effects_manager/presentation/controller/sounds_manager_controller.dart
+++ hisnelmoslem/lib/src/features/effects_manager/presentation/controller/sounds_manager_controller.dart
@@ -1,110 +1,26 @@
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/services.dart';
-import 'package:get/state_manager.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:hisnelmoslem/src/core/values/constant.dart';
+import 'package:hisnelmoslem/src/core/repos/app_data.dart';
 import 'package:vibration/vibration.dart';
 
-class SoundsManagerController extends GetxController {
-  /* *************** Variables *************** */
-  final box = GetStorage(kAppStorageKey);
-
-  /////////////////////////////
-  /// Sounds
-
-  /// get Tally Sound mode
-  bool get isTallySoundAllowed => box.read('tally_sound') ?? false;
-
-  /// set Tally Sound mode
-  void changeTallySoundStatus({required bool value}) =>
-      box.write('tally_sound', value);
-
-  /// get Tally Done Sound mode
-  bool get isZikrDoneSoundAllowed => box.read('zikr_done_sound') ?? false;
-
-  /// set Tally Done Sound mode
-  void changeZikrDoneSoundStatus({required bool value}) =>
-      box.write('zikr_done_sound', value);
-
-  /// get Tally Transition Sound mode
-  bool get isTransitionSoundAllowed =>
-      box.read('tally_transition_sound') ?? false;
-
-  /// set Tally  Transition Sound mode
-  void changeTransitionSoundStatus({required bool value}) =>
-      box.write('tally_transition_sound', value);
-
-  /// get Tally Finished Sound mode
-  bool get isAllAzkarFinishedSoundAllowed =>
-      box.read('all_azkar_finished_sound') ?? false;
-
-  /// set Tally Finished Sound mode
-  void changeAllAzkarFinishedSoundStatus({required bool value}) =>
-      box.write('all_azkar_finished_sound', value);
-
-  /////////////////////////////
-  /// Vibration
-
-  /// get Sound Effect Volume
-  double get soundEffectVolume => box.read('soundEffectVolume') ?? 1;
-
-  /// set Tally Vibrate mode
-  void changeSoundEffectVolume(double value) =>
-      box.write('soundEffectVolume', value);
-
-  /// get Tally Vibrate mode
-  bool get isTallyVibrateAllowed => box.read('tally_vibrate') ?? false;
-
-  /// set Tally Vibrate mode
-  void changeTallyVibrateStatus({required bool value}) =>
-      box.write('tally_vibrate', value);
-
-  /// get Tally Done Vibrate mode
-  bool get isZikrDoneVibrateAllowed => box.read('zikr_done_vibrate') ?? false;
-
-  /// set Tally Done Vibrate mode
-  void changeZikrDoneVibrateStatus({required bool value}) =>
-      box.write('zikr_done_vibrate', value);
-
-  /// get Tally Transition Vibrate mode
-  bool get isTransitionVibrateAllowed =>
-      box.read('tally_transition_vibrate') ?? false;
-
-  /// set Tally  Transition Vibrate mode
-  void changeTransitionVibrateStatus({required bool value}) =>
-      box.write('tally_transition_vibrate', value);
-
-  /// get Tally Finished Vibrate mode
-  bool get isAllAzkarFinishedVibrateAllowed =>
-      box.read('all_azkar_finished_vibrate') ?? false;
-
-  /// set Tally Finished Vibrate mode
-  void changeAllAzkarFinishedVibrateStatus({required bool value}) =>
-      box.write('all_azkar_finished_vibrate', value);
-
+class SoundsManagerController {
   ///
   final player = AudioPlayer();
 
-/* *************** Controller life cycle *************** */
-
-  /* *************** Functions *************** */
-  //
-
-  /////////////////////
   /// Play Sound
 
   Future<void> simulateTallySound() async {
     // player.play('sounds/tally_sound.mp3');
     await player.play(
       AssetSource('sounds/tally_sound.mp3'),
-      volume: soundEffectVolume,
+      volume: AppData.instance.soundEffectVolume,
     );
   }
 
   Future<void> simulateZikrDoneSound() async {
     await player.play(
       AssetSource('sounds/zikr_done_sound.mp3'),
-      volume: soundEffectVolume,
+      volume: AppData.instance.soundEffectVolume,
     );
   }
 
@@ -113,7 +29,7 @@ class SoundsManagerController extends GetxController {
   Future<void> simulateAllAzkarSoundFinished() async {
     await player.play(
       AssetSource('sounds/all_azkar_finished_sound.mp3'),
-      volume: soundEffectVolume,
+      volume: AppData.instance.soundEffectVolume,
     );
   }
 
@@ -163,37 +79,37 @@ class SoundsManagerController extends GetxController {
 
   //////////////////////////////
   Future playTallyEffects() async {
-    if (isTallySoundAllowed) {
+    if (AppData.instance.isTallySoundAllowed) {
       await simulateTallySound();
     }
-    if (isTallyVibrateAllowed) {
+    if (AppData.instance.isTallyVibrateAllowed) {
       await simulateTallyVibrate();
     }
   }
 
   Future playZikrDoneEffects() async {
-    if (isZikrDoneSoundAllowed) {
+    if (AppData.instance.isZikrDoneSoundAllowed) {
       await simulateZikrDoneSound();
     }
-    if (isZikrDoneVibrateAllowed) {
+    if (AppData.instance.isZikrDoneVibrateAllowed) {
       await simulateZikrDoneVibrate();
     }
   }
 
   Future playTransitionEffects() async {
-    if (isTransitionSoundAllowed) {
+    if (AppData.instance.isTransitionSoundAllowed) {
       await simulateTransitionSound();
     }
-    if (isTransitionVibrateAllowed) {
+    if (AppData.instance.isTransitionVibrateAllowed) {
       await simulateTransitionVibrate();
     }
   }
 
   Future playAllAzkarFinishedEffects() async {
-    if (isAllAzkarFinishedSoundAllowed) {
+    if (AppData.instance.isAllAzkarFinishedSoundAllowed) {
       await simulateAllAzkarSoundFinished();
     }
-    if (isAllAzkarFinishedVibrateAllowed) {
+    if (AppData.instance.isAllAzkarFinishedVibrateAllowed) {
       await simulateAllAzkarVibrateFinished();
     }
   }
