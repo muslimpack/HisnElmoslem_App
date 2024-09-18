df710421bf6ec713aa2a34ca5084a8ff8e6fddf0
7eltantawy <hasaneltantawy@gmail.com>
2024-09-19 01:08:04 +0300

    Update extension.dart

--- hisnelmoslem/lib/src/core/extensions/extension.dart
+++ hisnelmoslem/lib/src/core/extensions/extension.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 extension BuildContextExt on BuildContext {
-  void push(Widget route) {
-    Navigator.of(this).push(
+  Future<T?> push<T extends Object?>(Widget route) async {
+    return Navigator.of(this).push(
       MaterialPageRoute(
         builder: (context) {
           return route;
