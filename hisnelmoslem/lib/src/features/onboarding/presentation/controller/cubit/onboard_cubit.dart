f25c60a30bde8f47cfaf0656ce66f89fc351494d
7eltantawy <hasaneltantawy@gmail.com>
2024-09-19 16:05:50 +0300

    Update onboard_cubit.dart

--- hisnelmoslem/lib/src/features/onboarding/presentation/controller/cubit/onboard_cubit.dart
+++ hisnelmoslem/lib/src/features/onboarding/presentation/controller/cubit/onboard_cubit.dart
@@ -3,6 +3,7 @@ import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:hisnelmoslem/src/core/shared/widgets/empty.dart';
 import 'package:hisnelmoslem/src/core/utils/volume_button_manager.dart';
+import 'package:hisnelmoslem/src/core/values/constant.dart';
 import 'package:hisnelmoslem/src/features/settings/data/repository/app_settings_repo.dart';
 
 part 'onboard_state.dart';
@@ -44,29 +45,62 @@ class OnboardCubit extends Cubit<OnboardState> {
   ///TODO: Change every release
   List<Empty> get pageData {
     return [
-//     const Empty(
-//       title: "حصن المسلم الإصدار $appVersion",
-//       description: '''
-// السلام عليكم أيها المبارك
-// أهلا بك في تحديث جديد من حصن المسلم
-// قم بسحب الشاشة لتقليب الصفحات
-// أو استخدم مفاتيح الصوت لرؤية الميزات الجديدة
-// ''',
-//     ),
       const Empty(
-        title: "الجديد في هذا الإصدار",
+        title: "حصن المسلم الإصدار $appVersion",
+        description: '''
+السلام عليكم أيها الكريم
+أهلا بك في تحديث جديد من حصن المسلم
+قم بسحب الشاشة لتقليب الصفحات
+أو استخدم مفاتيح الصوت لرؤية الميزات الجديدة
+''',
+      ),
+      const Empty(
+        title: "الميزات الجديدة",
         isImage: false,
         icon: Icons.new_releases,
         description: """
-- يمكنك الآن التحكم في استخدام الأرقام الهندية من الإعدادات
-
-- التحكم في إبقاء الشاشة نشطة أثناء الذكر من الإعدادات
-
-- تحسين على القائمة الجانبية للواجهة الرئيسية
-
-- تحسين نافذة ضبط التنبيهات
-
-- شاشة إعدادات ألوان مشاركة الذكر كصورة يمكن الوصول إليها الآن من الشريط العلوي بجواز زر المشاركة بدلا من النافذة العائمة
+- دعم منصات الويندز
+- إمكانية استكمال جلسة الذكر السابقة
+- تصفية الأذكار من خلال المصدر والحكم 
+- تصفية الأبواب من خلال معدل التكرار (يومي - أسبوعي - شهري - سنوي)
+- إمكانية التغيير للوضع الليلي مباشرة من الأذكار
+- زر لإعادة تعيين الأذكار في وضع الصفحات والبطاقات
+- التحكم في التسبيح من خلال أزرار الصوت من خلال الإعدادات
+- إضافة خط جديد Roboto
+""",
+      ),
+      const Empty(
+        title: "التحسينات",
+        isImage: false,
+        icon: Icons.more_horiz,
+        description: """
+- تحسين على السبحة
+- إمكانية الانتقال للذكر التالي حتى بعد انتهاء الذكر
+- صفحة البحث عن الأبواب
+- بطاقات وشكل السبحة
+- بطاقات الذكر المفضل على الواجهة
+- تفعيل المؤثرات لبطاقات الذكر المفضل على الواجهة
+- النوافذ المنبثقة
+- عناصر الإدخال
+- تعطيل رؤية التطبيق بعد قفل الشاشة
+""",
+      ),
+      const Empty(
+        title: "الأعطال التي تم حلها",
+        isImage: false,
+        icon: Icons.bug_report_outlined,
+        description: """
+- عداد الإشعارات
+- عند الضغط على منبه لذكر يذهب لذكر آخر
+- تصويب بعض الأخطاء الإملائية
+""",
+      ),
+      const Empty(
+        title: "أخري",
+        isImage: false,
+        icon: Icons.more_horiz_rounded,
+        description: """
+- حذف تاريخ التحديثات
 """,
       ),
     ];
