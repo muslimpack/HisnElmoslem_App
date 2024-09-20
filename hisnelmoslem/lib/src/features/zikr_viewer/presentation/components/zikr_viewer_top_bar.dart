d82f767d65c5e2d9cbd232dfb7b3d4c9ab756e89
7eltantawy <hasaneltantawy@gmail.com>
2024-09-20 21:19:44 +0300

    refractor: move CommentaryDialog  to zikr viewer feature

--- hisnelmoslem/lib/src/features/zikr_viewer/presentation/components/zikr_viewer_top_bar.dart
+++ hisnelmoslem/lib/src/features/zikr_viewer/presentation/components/zikr_viewer_top_bar.dart
@@ -3,9 +3,9 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:hisnelmoslem/generated/l10n.dart';
 import 'package:hisnelmoslem/src/core/extensions/extension_object.dart';
 import 'package:hisnelmoslem/src/core/extensions/extension_platform.dart';
-import 'package:hisnelmoslem/src/core/shared/dialogs/commentary_dialog.dart';
 import 'package:hisnelmoslem/src/features/home/presentation/components/side_menu/toggle_brightness_btn.dart';
 import 'package:hisnelmoslem/src/features/zikr_viewer/data/models/zikr_content.dart';
+import 'package:hisnelmoslem/src/features/zikr_viewer/presentation/components/commentary_dialog.dart';
 import 'package:hisnelmoslem/src/features/zikr_viewer/presentation/controller/bloc/zikr_viewer_bloc.dart';
 import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 
