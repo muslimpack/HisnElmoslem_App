82650295ab46fcb31c14cfb037e66fcbbc8bdc84
7Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-06-17 18:50:47 +0300

    UI: improve tally card

--- hisnelmoslem/lib/src/features/tally/presentation/components/tally_card.dart
+++ hisnelmoslem/lib/src/features/tally/presentation/components/tally_card.dart
@@ -24,99 +24,102 @@ class TallyCard extends StatelessWidget {
     } else {
       isActivated = false;
     }
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Column(
-          mainAxisSize: MainAxisSize.min,
+    return Container(
+      decoration: BoxDecoration(
+        border: BoxBorder.fromLTRB(
+          right: BorderSide(
+            width: 10,
+            color: isActivated
+                ? Theme.of(context).colorScheme.primary
+                : Colors.transparent,
+          ),
+        ),
+      ),
+      child: ListTile(
+        isThreeLine: true,
+        tileColor: isActivated
+            ? Theme.of(
+                context,
+              ).colorScheme.primary.withAlpha((.2 * 255).round())
+            : null,
+        onTap: () {
+          context.read<TallyBloc>().add(
+            TallyToggleCounterActivationEvent(
+              counter: dbTally,
+              activate: !isActivated,
+            ),
+          );
+        },
+        title: Text(
+          dbTally.title,
+          style: Theme.of(context).textTheme.displayMedium,
+        ),
+        subtitle: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            ListTile(
-              isThreeLine: true,
-              tileColor: isActivated
-                  ? Theme.of(
-                      context,
-                    ).colorScheme.primary.withAlpha((.2 * 255).round())
-                  : null,
-              onTap: () {
-                context.read<TallyBloc>().add(
-                  TallyToggleCounterActivationEvent(
-                    counter: dbTally,
-                    activate: !isActivated,
-                  ),
-                );
-              },
-              leading: Icon(
-                isActivated ? Icons.done_all_outlined : null,
-                size: 40,
-              ),
-              title: Text(dbTally.title),
-              subtitle: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  Text(
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                Expanded(
+                  child: Text(
                     DateFormat(kDateTimeHumanFormat).format(dbTally.lastUpdate),
                   ),
-                  Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      IconButton(
-                        tooltip: S.of(context).edit,
-                        onPressed: () async {
-                          final EditorResult<DbTally>? result =
-                              await showTallyEditorDialog(
-                                context: context,
-                                dbTally: dbTally,
-                              );
+                ),
+                IconButton(
+                  tooltip: S.of(context).edit,
+                  onPressed: () async {
+                    final EditorResult<DbTally>? result =
+                        await showTallyEditorDialog(
+                          context: context,
+                          dbTally: dbTally,
+                        );
 
-                          if (result == null || !context.mounted) return;
-                          switch (result.action) {
-                            case EditorActionEnum.edit:
-                              context.read<TallyBloc>().add(
-                                TallyEditCounterEvent(counter: result.value),
-                              );
-                            case EditorActionEnum.delete:
-                              context.read<TallyBloc>().add(
-                                TallyDeleteCounterEvent(counter: result.value),
-                              );
-                            default:
-                          }
-                        },
-                        icon: const Icon(Icons.edit),
-                      ),
-                      IconButton(
-                        tooltip: S.of(context).delete,
-                        onPressed: () async {
-                          final bool? confirm = await showDialog(
-                            context: context,
-                            builder: (_) {
-                              return YesOrNoDialog(
-                                msg: S.of(context).counterWillBeDeleted,
-                              );
-                            },
-                          );
+                    if (result == null || !context.mounted) return;
+                    switch (result.action) {
+                      case EditorActionEnum.edit:
+                        context.read<TallyBloc>().add(
+                          TallyEditCounterEvent(counter: result.value),
+                        );
+                      case EditorActionEnum.delete:
+                        context.read<TallyBloc>().add(
+                          TallyDeleteCounterEvent(counter: result.value),
+                        );
+                      default:
+                    }
+                  },
+                  icon: const Icon(Icons.edit),
+                ),
+                IconButton(
+                  tooltip: S.of(context).delete,
+                  onPressed: () async {
+                    final bool? confirm = await showDialog(
+                      context: context,
+                      builder: (_) {
+                        return YesOrNoDialog(
+                          msg: S.of(context).counterWillBeDeleted,
+                        );
+                      },
+                    );
 
-                          if (confirm == null || !confirm || !context.mounted) {
-                            return;
-                          }
+                    if (confirm == null || !confirm || !context.mounted) {
+                      return;
+                    }
 
-                          context.read<TallyBloc>().add(
-                            TallyDeleteCounterEvent(counter: dbTally),
-                          );
-                        },
-                        icon: const Icon(Icons.delete),
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              trailing: Text(
-                dbTally.count.toString().toArabicNumber(),
-                style: const TextStyle(fontSize: 15),
-              ),
+                    context.read<TallyBloc>().add(
+                      TallyDeleteCounterEvent(counter: dbTally),
+                    );
+                  },
+                  icon: const Icon(Icons.delete),
+                ),
+              ],
             ),
           ],
         ),
-      ],
+        trailing: Text(
+          dbTally.count.toString().toArabicNumber(),
+          style: const TextStyle(fontSize: 15),
+        ),
+      ),
     );
   }
 }
