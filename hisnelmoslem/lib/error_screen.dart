6122a4179e692d3efcfc5ec4b0a21a1a7c6ec5d5
7Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-06-28 06:18:32 +0300

    Add email error reporting with attachment
    Integrated the flutter_email_sender package to allow users to send detailed error reports via email, including an attached error report file. Refactored error screen to generate and attach error details, and updated UI to use the new email functionality.

--- hisnelmoslem/lib/error_screen.dart
+++ hisnelmoslem/lib/error_screen.dart
@@ -1,25 +1,41 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_email_sender/flutter_email_sender.dart';
 import 'package:hisnelmoslem/generated/lang/app_localizations.dart';
+import 'package:hisnelmoslem/src/core/extensions/extension_datetime.dart';
+import 'package:hisnelmoslem/src/core/functions/open_url.dart';
+import 'package:hisnelmoslem/src/core/functions/print.dart';
 import 'package:hisnelmoslem/src/core/values/constant.dart';
-import 'package:url_launcher/url_launcher.dart';
+import 'package:path_provider/path_provider.dart';
 
 class ErrorScreen extends StatelessWidget {
   final FlutterErrorDetails details;
   const ErrorScreen({super.key, required this.details});
 
-  Future<void> _sendEmail(BuildContext context, String errorMessage) async {
-    const String subject = 'حصن المسلم | خلل في الأداء | $kAppVersion';
-    final String body = 'حدث الخطأ التالي:\n\n$errorMessage\n\n';
-    final Uri emailUri = Uri(
-      scheme: 'mailto',
-      path: kOrgEmail,
-      query: Uri.encodeFull('subject=$subject&body=$body'),
-    );
+  Future<void> emailTheError(
+    BuildContext context,
+    FlutterErrorDetails error,
+  ) async {
+    // get receiver
+    const String emailReceiver = kOrgEmail;
+    try {
+      final String fullErrorDetails = generateErrorReport(error);
 
-    if (await canLaunchUrl(emailUri)) {
-      await launchUrl(emailUri);
-    } else {
+      final String errorReportFilePath = await writeToTextFile(
+        fullErrorDetails,
+      );
+      final Email email = Email(
+        body: 'حدث خطأ غير متوقع أثناء استخدام التطبيق.',
+        subject: 'حصن المسلم | خلل في الأداء | $kAppVersion',
+        recipients: [emailReceiver],
+        attachmentPaths: [errorReportFilePath],
+      );
+      await FlutterEmailSender.send(email);
+    } catch (e) {
+      hisnPrint(e.toString());
+      openURL('mailto:$emailReceiver');
       if (!context.mounted) return;
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
@@ -28,6 +44,38 @@ class ErrorScreen extends StatelessWidget {
         ),
       );
     }
+  }
+
+  String generateErrorReport(FlutterErrorDetails error) {
+    final lineBreaker = '\n${'-' * 40}\n';
+    final StringBuffer buffer = StringBuffer();
+
+    ///
+    buffer.writeln(DateTime.now().humanize);
+    buffer.writeln(lineBreaker);
+
+    ///
+    buffer.writeln('App Version: $kAppVersion');
+    buffer.writeln('Operating System: ${Platform.operatingSystem}');
+    buffer.writeln(lineBreaker);
+
+    ///
+    buffer.writeln('The exception was:\n');
+    buffer.writeln(error.exception);
+    buffer.writeln(lineBreaker);
+
+    ///
+    buffer.writeln('Stack Trace:\n');
+    buffer.writeln(error.stack);
+
+    return buffer.toString();
+  }
+
+  Future<String> writeToTextFile(String text) async {
+    final Directory directory = await getTemporaryDirectory();
+    final File file = File('${directory.path}/ErrorReport.txt');
+    await file.writeAsString(text);
+    return file.path;
   }
 
   void _copyToClipboard(BuildContext context, String text) {
@@ -79,20 +127,20 @@ class ErrorScreen extends StatelessWidget {
                   ),
                 ),
               ),
-              Wrap(
+              Row(
                 spacing: 10,
-                runSpacing: 10,
-                alignment: WrapAlignment.center,
                 children: [
-                  ElevatedButton.icon(
+                  Expanded(
+                    child: ElevatedButton.icon(
+                      onPressed: () => emailTheError(context, details),
+                      icon: const Icon(Icons.email),
+                      label: Text(S.of(context).sendEmail),
+                    ),
+                  ),
+                  TextButton.icon(
                     onPressed: () => _copyToClipboard(context, errorMessage),
                     icon: const Icon(Icons.copy),
                     label: Text(S.of(context).copy),
-                  ),
-                  ElevatedButton.icon(
-                    onPressed: () => _sendEmail(context, errorMessage),
-                    icon: const Icon(Icons.email),
-                    label: Text(S.of(context).sendEmail),
                   ),
                 ],
               ),
