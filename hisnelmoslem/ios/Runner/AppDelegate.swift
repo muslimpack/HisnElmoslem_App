489841bd8b77d1428d582d8599b92e010a70a022
Hassan Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2025-12-26 00:55:00 +0200

    App first run

--- hisnelmoslem/ios/Runner/AppDelegate.swift
+++ hisnelmoslem/ios/Runner/AppDelegate.swift
@@ -1,22 +1,13 @@
 import UIKit
 import Flutter
-import awesome_notifications
-import shared_preferences_ios
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
-
-    SwiftAwesomeNotificationsPlugin.setPluginRegistrantCallback { registry in
-          SwiftAwesomeNotificationsPlugin.register(
-            with: registry.registrar(forPlugin: "io.flutter.plugins.awesomenotifications.AwesomeNotificationsPlugin")!)
-          FLTSharedPreferencesPlugin.register(
-            with: registry.registrar(forPlugin: "io.flutter.plugins.sharedpreferences.SharedPreferencesPlugin")!)
-      }
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
