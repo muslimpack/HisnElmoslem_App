9f1ef1fe7b9c2d9d5b103c59668fe9c531036d10
Hassan Eltantawy <50374022+7Eltantawy@users.noreply.github.com>
2026-02-21 11:17:16 +0200

    Update text_field.dart

--- hisnelmoslem/lib/src/core/shared/custom_inputs/text_field.dart
+++ hisnelmoslem/lib/src/core/shared/custom_inputs/text_field.dart
@@ -1,38 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:hisnelmoslem/src/core/shared/custom_inputs/custom_field_decoration.dart';
 
-class UserTextField extends StatelessWidget {
-  final bool autoFocus;
-  final TextEditingController controller;
-  final String hintText;
-  final Function(String)? onChange;
-
-  const UserTextField({
-    super.key,
-    required this.controller,
-    required this.hintText,
-    this.onChange,
-    this.autoFocus = false,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: TextField(
-        autofocus: autoFocus,
-        textAlign: TextAlign.center,
-        controller: controller,
-        onChanged: onChange,
-        decoration: customInputDecoration.copyWith(
-          hintText: hintText,
-          labelText: hintText,
-        ),
-      ),
-    );
-  }
-}
-
 class UserTextFormField extends StatelessWidget {
   final bool autoFocus;
   final TextEditingController controller;
@@ -58,10 +26,7 @@ class UserTextFormField extends StatelessWidget {
         textAlign: TextAlign.center,
         controller: controller,
         onChanged: onChange,
-        decoration: customInputDecoration.copyWith(
-          hintText: hintText,
-          labelText: hintText,
-        ),
+        decoration: customInputDecoration.copyWith(hintText: hintText, labelText: hintText),
         validator: validator,
       ),
     );
